8e5060aa6a7292c610148e7bc66644956da41980
miraz66 <mirajulislam0509@gmail.com>
2025-06-04 23:42:57 +0600

    Add Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase-clint.js";
 import Todo from "./components/Todo.jsx";
-import Test from "./components/Test.jsx";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -20,43 +19,75 @@ function App() {
     }
   };
 
-  console.log(todoList);
   const handleAddTodo = async () => {
+    if (!newTodo.trim()) return;
+
     const newTodoData = {
       name: newTodo,
-      is_completed: false
+      is_completed: false,
     };
 
     const { data, error } = await supabase
-        .from("TodoList")
-        .insert([newTodoData])
-        .single();
+      .from("TodoList")
+      .insert([newTodoData])
+      .single();
 
     if (error) {
       console.log("Error adding todo:", error);
     } else {
-      setTodoList((prev)=>[...prev, data]);
+      setTodoList((prev) => [...prev, data]);
       setNewTodo("");
     }
   };
 
+  const handleToggleTodo = async (id) => {
+    const current = todoList.find((todo) => todo.id === id);
+    if (!current) return;
+
+    const { error } = await supabase
+      .from("TodoList")
+      .update({ is_completed: !current.is_completed })
+      .eq("id", id);
+
+    if (!error) {
+      setTodoList((prev) =>
+        prev.map((todo) =>
+          todo.id === id ? { ...todo, is_completed: !todo.is_completed } : todo,
+        ),
+      );
+    }
+  };
 
   const handleDeleteTodo = async (id) => {
-    const { error } = await supabase
-        .from("TodoList")
-        .delete()
-        .match({ id });
-    if (error) {
-      console.log("Error deleting todo:", error);
-    } else {
+    const { error } = await supabase.from("TodoList").delete().eq("id", id);
+    if (!error) {
       setTodoList((prev) => prev.filter((todo) => todo.id !== id));
     }
   };
 
+  const handleEditTodo = async (id, name) => {
+    const { error } = await supabase
+      .from("TodoList")
+      .update({ name })
+      .eq("id", id);
+
+    if (!error) {
+      setTodoList((prev) =>
+        prev.map((todo) => (todo.id === id ? { ...todo, name } : todo)),
+      );
+    }
+  };
+
   return (
-      <>
-        <Todo setNewTodo={setNewTodo} handleAddTodo={handleAddTodo} todoList={todoList} handleDeleteTodo={handleDeleteTodo}/>
-      </>
+    <Todo
+      newTodo={newTodo}
+      setNewTodo={setNewTodo}
+      handleAddTodo={handleAddTodo}
+      todoList={todoList}
+      handleDeleteTodo={handleDeleteTodo}
+      handleToggleTodo={handleToggleTodo}
+      handleEditTodo={handleEditTodo}
+    />
   );
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,46 +1,141 @@
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 import clsx from "clsx";
 
-export default function Todo({ setNewTodo, handleAddTodo, todoList, handleDeleteTodo}) {
+export default function Todo({
+  newTodo,
+  setNewTodo,
+  handleAddTodo,
+  todoList,
+  handleDeleteTodo,
+  handleToggleTodo,
+  handleEditTodo,
+}) {
+  const [showMore, setShowMore] = useState(null);
+  const [editedTodo, setEditedTodo] = useState(null);
+
+  const toggleShowMore = (id) => {
+    setShowMore((prev) => (prev === id ? null : id));
+  };
 
   return (
-      <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16">
-        <div className="px-4 py-2">
-          <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
-        </div>
-        <form className="w-full max-w-sm mx-auto px-4 py-2">
-          <div className="flex items-center border-b-2 border-teal-500 py-2">
-            <input
-                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                type="text"
-                placeholder="Add a task"
-                onChange={(e) => setNewTodo(e.target.value)}
-            />
-            <button
-                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                type="button"
-                onClick={handleAddTodo}
-            >
-              Add
-            </button>
-          </div>
-        </form>
-
-        <ul className="divide-y divide-gray-200 px-4">
-          {todoList?.map((todo) => (
-              <li key={todo.id} className="py-4 flex justify-between">
-                <div className="flex items-center">
-                  <input checked={todo.is_completed} id="todo1" name="todo1" type="checkbox"
-                         className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" readOnly/>
-                  <label htmlFor="todo1" className="ml-3 block text-gray-900">
-                    <span className={clsx("text-lg font-medium", todo.is_completed && "line-through")}>{todo.name}</span>
-                    <span className="text-xs font-light text-gray-500"> Due on 4/1/23</span>
-                  </label>
-                </div>
-
-                <button onClick={() => handleDeleteTodo(todo.id)} className="bg-red-500 text-sm cursor-pointer hover:bg-red-700 text-white font-medium py-1 px-2.5 rounded">Delete</button>
-              </li>
-          ))}
-        </ul>
+    <div className="mx-auto mt-16 max-w-md overflow-hidden rounded-lg bg-white shadow-lg">
+      <div className="px-4 py-2">
+        <h1 className="text-2xl font-bold text-gray-800 uppercase">
+          To-Do List
+        </h1>
       </div>
+
+      <form className="mx-auto w-full max-w-sm px-4 py-2">
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            className="mr-3 w-full appearance-none border-none bg-transparent px-2 py-1 leading-tight text-gray-700 focus:outline-none"
+            type="text"
+            placeholder="Add a task"
+            onChange={(e) => setNewTodo(e.target.value)}
+            value={newTodo}
+            required
+          />
+          <button
+            className="flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 px-2 py-1 text-sm text-white hover:border-teal-700 hover:bg-teal-700"
+            type="button"
+            onClick={handleAddTodo}
+          >
+            Add
+          </button>
+        </div>
+      </form>
+
+      <ul className="divide-y divide-gray-200 px-4">
+        {todoList?.map((todo) => {
+          if (!todo) return null;
+
+          return (
+            <li key={todo.id}>
+              <div className="flex justify-between py-4">
+                {editedTodo?.id === todo.id ? (
+                  <div className="flex w-full items-center">
+                    <input
+                      className="w-full appearance-none rounded border px-2 py-1 text-gray-700 focus:outline-none"
+                      type="text"
+                      onChange={(e) =>
+                        setEditedTodo({ ...editedTodo, name: e.target.value })
+                      }
+                      value={editedTodo.name}
+                      required
+                    />
+                  </div>
+                ) : (
+                  <div className="flex items-center">
+                    <input
+                      onChange={() => handleToggleTodo(todo.id)}
+                      checked={!!todo.is_completed}
+                      id={`todo-${todo.id}`}
+                      name={`todo-${todo.id}`}
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-teal-600 focus:ring-teal-500"
+                    />
+                    <label
+                      htmlFor={`todo-${todo.id}`}
+                      className="ml-3 block text-gray-900"
+                    >
+                      <span
+                        className={clsx(
+                          "text-lg font-medium",
+                          todo.is_completed && "line-through",
+                        )}
+                      >
+                        {todo.name}
+                      </span>
+                      <span className="text-xs font-light text-gray-500">
+                        {" "}
+                        Due on 4/1/23
+                      </span>
+                    </label>
+                  </div>
+                )}
+
+                {editedTodo?.id === todo.id ||
+                  (editedTodo === null && (
+                    <ChevronDownIcon
+                      className="h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-600"
+                      onClick={() => toggleShowMore(todo.id)}
+                      aria-hidden="true"
+                    />
+                  ))}
+              </div>
+
+              {showMore === todo.id && (
+                <div className="flex justify-end gap-2 pb-2">
+                  {editedTodo?.id === todo.id ? (
+                    <button
+                      onClick={() => {
+                        setEditedTodo(null);
+                      }}
+                      className="rounded bg-teal-500 px-2.5 py-1 text-sm text-white hover:bg-teal-700"
+                    >
+                      Cancel
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => setEditedTodo(todo)}
+                      className="rounded bg-teal-500 px-2.5 py-1 text-sm text-white hover:bg-teal-700"
+                    >
+                      Edit
+                    </button>
+                  )}
+                  <button
+                    onClick={() => handleDeleteTodo(todo.id)}
+                    className="rounded bg-red-500 px-2.5 py-1 text-sm text-white hover:bg-red-700"
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
-};
+}
